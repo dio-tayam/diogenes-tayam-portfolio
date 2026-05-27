3d0f325ea31d9a1a65ee8fb0e12be0605a58c64d
Diogenes Tayam <diogenesiiiaraojo.tayam@gmail.com>
2026-05-27 23:52:16 +0800

    Update profile.js

--- src/data/profile.js
+++ src/data/profile.js
@@ -196,16 +196,16 @@ export const profile = {
     },
   ],
   experience: [
+    // { 
+    //   role: "Full Stack Developer", 
+    //   company: "AppArara Digital", 
+    //   year: "January 2026 - Present", 
+    //   employmentType: "Part Time",
+    //   active: true,
+    //   description: "Developed a web-based tool for prediction market intelligence with wallet integration using React.js, Node.js, and PostgreSQL."
+    // },
     { 
       role: "Full Stack Developer", 
-      company: "AppArara Digital", 
-      year: "January 2026 - Present", 
-      employmentType: "Part Time",
-      active: true,
-      description: "Developed a web-based tool for prediction market intelligence with wallet integration using React.js, Node.js, and PostgreSQL."
-    },
-    { 
-      role: "Junior Programmer", 
       company: "Bicol University - Information Communication Technology Office", 
       year: "October 2025 - Present", 
       employmentType: "Full Time",
